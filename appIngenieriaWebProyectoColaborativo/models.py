377c1882037474aa9e79f8e5622de9d955bd9f46
IkerCubillo <i.cubillo@opendeusto.es>
2024-10-28 13:49:26 +0100

    nuevas plantillas

--- appIngenieriaWebProyectoColaborativo/models.py
+++ appIngenieriaWebProyectoColaborativo/models.py
@@ -3,11 +3,20 @@ from django.db import models
 class Raza(models.Model):
     # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
     nombre = models.CharField(max_length=50)
-    telefono = models.IntegerField()
+    descripcion = models.CharField(max_length=2000)
+
 
     def __str__(self):
         return self.nombre
+    
+class SubRaza(models.Model):
+    raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=2000)
 
+    def __str__(self):
+        return self.nombre
+    
 class Pelicula(models.Model):
     # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
     nombre = models.CharField(max_length=50)
@@ -18,13 +27,18 @@ class Pelicula(models.Model):
 class Personaje(models.Model):
     # Campo para la relación one-to-many (un personaje pertenece a una raza)
     raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
+    subRaza = models.ForeignKey(SubRaza, on_delete=models.CASCADE)
     # Campo para la relación many-to-many (un personaje participa en varias peliculas)
-    pelicula = models.ManyToManyField(Pelicula)
+    peliculas = models.ManyToManyField(Pelicula)
+
     nombre = models.CharField(max_length=40)
+    edadTierra = models.CharField(max_length=50)
     fecha_nacimiento = models.DateField()
-    # Es posible indicar un valor por defecto mediante 'default'
+    fecha_muerte = models.DateField()
+    genero = models.CharField(max_length=50)
     antiguedad = models.IntegerField(default=0)
     # Para permitir propiedades con valor null, añadiremos las opciones null=True, blank=True.       	
 
     def __str__(self):
         return self.nombre
+    
--- appIngenieriaWebProyectoColaborativo/views.py
+++ appIngenieriaWebProyectoColaborativo/views.py
@@ -1,34 +1,36 @@
-from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import render
 from .models import Raza, Pelicula, Personaje
 
-
-#devuelve el listado de razas
+#devuelve el listado de empresas
 def index_razas(request):
 	razas = get_list_or_404(Raza.objects.order_by('nombre'))
-	output = ', '.join([d.nombre for d in razas])
-	return HttpResponse(output)
+	context = {'lista_razas': razas }
+	return render(request, 'index.html', context)
 
 #devuelve los datos de un raza
 def show_raza(request, raza_id):
-	raza = get_list_or_404(Raza.objects.get(pk=raza_id))
-	output = f'Detalles del raza: {raza.id}, {raza.nombre}, {raza.telefono}'
-	return HttpResponse(output)
+	raza = get_object_or_404(Raza, pk=raza_id)
+	context = {'raza': raza }
+	return render(request, 'detail.html', context)
 
-#devuelve los personajes de un raza
+#devuelve los empelados de un raza
 def index_personajes(request, raza_id):
-	raza = get_list_or_404(Raza.objects.get(pk=raza_id))
-	output = ', '.join([e.nombre for e in raza.personaje_set.all()])
-	return HttpResponse(output)
+	raza = get_object_or_404(Raza, pk=raza_id)
+	personajes =  raza.personaje_set.all()
+	context = {'raza': raza, 'personajes' : personajes }
+	return render(request, 'personajes.html', context)
 
 #devuelve los detalles de un personaje
 def show_personaje(request, personaje_id):
-	personaje = get_list_or_404(Personaje.objects.get(pk=personaje_id))
-	output = f'Detalles del personaje: {personaje.id}, {personaje.nombre}, {personaje.fecha_nacimiento}, {personaje.antiguedad}, {str(personaje.raza)}. Peliculas: {[h.nombre for h in personaje.peliculaes.all()]}'
-	return HttpResponse(output)
+	personaje = get_object_or_404(Personaje, pk=personaje_id)
+	peliculaes =  personaje.peliculas.all()
+	context = { 'personaje': personaje, 'peliculas' : peliculaes }
+	return render(request, 'personaje.html', context)
 
-#devuelve los detalles de una pelicula
+# Devuelve los detalles de una pelicula
 def show_pelicula(request, pelicula_id):
-	pelicula = get_list_or_404(Pelicula.objects.get(pk=pelicula_id))
-	output = f'Detalles de la pelicula: {pelicula.id}, {pelicula.nombre}. Personajes: {[e.nombre for e in pelicula.personaje_set.all()]}'
-	return HttpResponse(output)
+    pelicula = get_object_or_404(Pelicula, pk=pelicula_id)
+    personajes =  pelicula.personaje_set.all()
+    context = { 'personajes': personajes, 'pelicula' : pelicula }
+    return render(request, 'pelicula.html', context)
