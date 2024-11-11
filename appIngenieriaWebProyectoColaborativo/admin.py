42e1a4def2db3d46b28530c713651d05337783a5
IkerCubillo <i.cubillo@opendeusto.es>
2024-11-11 13:54:10 +0100

    arreglado problemas modelos base de datos

--- appIngenieriaWebProyectoColaborativo/admin.py
+++ appIngenieriaWebProyectoColaborativo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pelicula, Personaje, Raza
+from .models import Pelicula, Personaje, Raza, SubRaza
 
 admin.site.register(Pelicula)
 admin.site.register(Personaje)
 admin.site.register(Raza)
+admin.site.register(SubRaza)
