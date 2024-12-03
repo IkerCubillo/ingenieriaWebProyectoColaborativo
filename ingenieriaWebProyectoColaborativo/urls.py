f0aa04a1e2a637a15ffb812e4dbaaa700ef0e2dd
IkerCubillo <i.cubillo@opendeusto.es>
2024-12-03 13:41:30 +0100

    agenda Vue

--- ingenieriaWebProyectoColaborativo/urls.py
+++ ingenieriaWebProyectoColaborativo/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
- path('appIngenieriaWebProyectoColaborativo/', include('appIngenieriaWebProyectoColaborativo.urls')),
+ path('', include('appIngenieriaWebProyectoColaborativo.urls')),
  path('admin/', admin.site.urls),
 ]
