35c910cec23b92c9907800f77e845e72b68b96d8
IkerCubillo <i.cubillo@opendeusto.es>
2024-12-10 14:29:55 +0100

    Cambios para permitir CSS

--- ingenieriaWebProyectoColaborativo/settings.py
+++ ingenieriaWebProyectoColaborativo/settings.py
@@ -154,6 +154,7 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- ingenieriaWebProyectoColaborativo/urls.py
+++ ingenieriaWebProyectoColaborativo/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
@@ -21,6 +23,10 @@ from django.views.i18n import set_language
 
 urlpatterns = [
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
