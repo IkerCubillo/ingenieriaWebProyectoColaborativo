649d34d2923ec0d3ce7f70a1117368626b94ce7c
IkerCubillo <i.cubillo@opendeusto.es>
2024-12-10 14:00:24 +0100

    cambios settings

--- ingenieriaWebProyectoColaborativo/settings.py
+++ ingenieriaWebProyectoColaborativo/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#1fjy=yz3)5t$0+mdl4#ws&yyo$w1e!q(9dg@27=0g!0ada@+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.render.com', "127.0.0.1"]
+ALLOWED_HOSTS = ['.render.com', "127.0.0.1", "https://ingenieriaweb-proyecto-iker-cubillo.onrender.com"]
 
 
 # Application definition
